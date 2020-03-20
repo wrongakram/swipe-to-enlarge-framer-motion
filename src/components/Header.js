09e04995cb7c6b8660f60e4f1e3b0bdf541ecb23
Akram Khalid <akramkhalid12@gmail.com>
2020-03-20 16:59:21 -0500

    Starter files

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,15 @@
 import React from "react";
+import Logo from "../assets/logo.png";
 
 const Breadcrumb = () => {
   return (
-    <div className="header">
-      <div className="header-inner">
-        <div className="header-content">
-          <div className="logo">LOGO</div>
-          <div className="hamburger-menu">
+    <div className='header'>
+      <div className='header-inner'>
+        <div className='header-content'>
+          <div className='logo'>
+            <img src={Logo} alt='Logo' />
+          </div>
+          <div className='hamburger-menu'>
             <span></span>
             <span></span>
             <span></span>
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,11 +1,5 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import ProductImage from "../assets/product.png";
-import {
-  motion,
-  useSpring,
-  useTransform,
-  AnimatePresence
-} from "framer-motion";
 
 //svgs
 import { ReactComponent as Close } from "../assets/close.svg";
@@ -13,48 +7,11 @@ import { ReactComponent as Chevron } from "../assets/chevron.svg";
 import { ReactComponent as DownArrow } from "../assets/down-arrow.svg";
 
 const Product = () => {
-  const ease = [0.6, 0.05, -0.01, 0.99];
-  let x = useSpring(0, { stiffness: 300, damping: 200, ease: ease });
-  const fadeIn = useTransform(x, [-100, 0], [1, 0]);
-  const fadeOut = useTransform(x, [-60, 0], [0, 1]);
-  const scale = useTransform(x, [-100, 0], [1.25, 1]);
-  const width = useTransform(x, [-1060, 0], [350, 0]);
-  const up = useTransform(x, [-100, 0], [-100, 0]);
-  const down = useTransform(x, [-100, 0], [100, 0]);
-
-  //state
-  const [state, setState] = useState(false);
-
-  //scrolltargets
-  let targetElement = document.querySelector("html");
-
-  // Update the state to check if the user has dragged the product
-  useEffect(() => {
-    x.onChange(() => {
-      x.get() > -100 ? setState(false) : setState(true);
-    });
-  }, [x]);
-
-  //Setting body scroll
-  useEffect(() => {
-    state
-      ? targetElement.classList.add("no-scroll")
-      : targetElement.classList.remove("no-scroll");
-  });
-
-  // Closing the drag product
-  const closeProductDrag = () => {
-    x.stop();
-    x.set(0);
-  };
-
   return (
     <div className='product'>
       <div className='product-inner'>
         <div className='product-content'>
-          <motion.div
-            style={{ translateY: up }}
-            className='product-content-inner'>
+          <div className='product-content-inner'>
             <h4>Freedom Everywhere</h4>
             <h1>HiFive1 Rev B</h1>
             <p>
@@ -66,55 +23,35 @@ const Product = () => {
               <button>Buy Now ($59)</button>
               <DownArrow />
             </div>
-          </motion.div>
+          </div>
         </div>
       </div>
       <div className='product-slide-enlarge'>
-        <motion.div
-          className='background'
-          style={{ opacity: fadeIn }}></motion.div>
-        {state ? (
-          <AnimatePresence>
-            <motion.div
-              initial={{ y: -30, opacity: 0 }}
-              animate={{ y: 0, opacity: 1 }}
-              exit={{ y: -30, opacity: 0 }}
-              transition={{ ease: ease }}
-              className='product-drag-header'>
-              <div className='company-name'>HiFive1</div>
-              <div onClick={closeProductDrag} className='close'>
-                <Close />
-              </div>
-            </motion.div>
-          </AnimatePresence>
-        ) : (
-          <AnimatePresence></AnimatePresence>
-        )}
-        <div className='product-container'>
-          <motion.div
-            drag='x'
-            style={{ x, scale }}
-            dragElastic={0.05}
-            dragConstraints={{ left: -1060, right: 0 }}
-            className='product-image'>
-            <img src={ProductImage} alt='product' />
-          </motion.div>
+        <div className='background'></div>
+        <div className='product-drag-header'>
+          <div className='company-name'>HiFive1</div>
+          <div className='close'>
+            <Close />
+          </div>
         </div>
-        <motion.div style={{ paddingBottom: down }} className='product-drag'>
+        <div className='product-container'>
+          <div className='product-image'>
+            <img src={ProductImage} alt='product' />
+          </div>
+        </div>
+        <div className='product-drag'>
           <div className='product-drag-inner'>
             <div className='product-drag-label'>
-              <motion.h6 style={{ x, opacity: fadeOut }}>
+              <h6>
                 <Chevron />
                 Drag To Enlarge
-              </motion.h6>
+              </h6>
             </div>
             <div className='product-drag-progress-background'>
-              <motion.div
-                style={{ width }}
-                className='product-drag-progress'></motion.div>
+              <div className='product-drag-progress'></div>
             </div>
           </div>
-        </motion.div>
+        </div>
       </div>
     </div>
   );
